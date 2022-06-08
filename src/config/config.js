6c3d087c1f12336617c0ba32da08ede02eb31a04
suhriar <mochamad.suhri@gmail.com>
2022-06-08 22:12:02 +0700

    config update

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME, DB_DIALECT } =
+const { DB_USERNAME, DB_PASSWORD, DB_HOSTNAME, DB_NAME, DB_DIALECT } =
   process.env;
 
 module.exports = {
@@ -7,7 +7,7 @@ module.exports = {
         username: DB_USERNAME,
         password: DB_PASSWORD,
         database: DB_NAME,
-        host: DB_HOST,
+        host: DB_HOSTNAME,
         dialect: DB_DIALECT,
     },
     test: {
@@ -21,7 +21,7 @@ module.exports = {
         username: DB_USERNAME,
         password: DB_PASSWORD,
         database: DB_NAME,
-        host: DB_HOST,
+        host: DB_HOSTNAME,
         dialect: DB_DIALECT,
     },
 };
